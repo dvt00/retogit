a71a4a5e76cf323ae322b2aee2277bccbf91ef98
dvt00 <dvt00@hotmail.es>
2024-05-12 18:31:00 +0200

    Add files via upload
    Añadido src java

---
+++ src/ItemBiblioteca.java
@@ -0,0 +1,15 @@
+public class ItemBiblioteca {
+    public static void ItemBibliteca(String[] args) {
+
+        String titulo;{
+        int titulo="ElSeñordeLosAnillos";}
+        String autor;{
+        int autor="J.R.R. TOLKIEN";}
+        String anioPublicacion;{
+        int anioPublicacion="1954";}
+        String numeroPaginas;{
+        int numeroPaginas="1216";}
+    }}
+
+
+
---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        String titulo="ElSeñordeLosAnillos";
+        String autor="J.R.R. TOLKIEN";
+        String anioPublicacion="1954";}
+        String numeroPaginas="1216";
+    int anioPublicacion=1954;
+    int paginas=1216;
+}
+        System.out.println("Libro:"+(String titulo)+"Autor:"+(String autor)+"Año de publicación:"+anioPublicacion+"Número de páginas:"+(String numeroPaginas));
+    }
+}
